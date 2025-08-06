e4da994031c8dc2740f83e18de337bebefc8b61c
Umang Shukla <aapkadb@gmail.com>
2025-08-07 00:54:11 +0530

    Video Day 6

---
+++ Python/Week-1.py
@@ -0,0 +1,8 @@
+# This is Single Line Comment
+print("Day 5 Comment")
+'''
+Multi Line Comment Triple Single Quote.
+'''
+print("Escape \nSequence Characted")
+
+print("Hey",6,7,sep="*",end="END\n")
